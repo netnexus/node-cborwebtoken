f0acc66363f6bb4fcfdfe72ea809bda1167bf0fb
Lukas Dietrich <lukasdietrich@Iris.fritz.box>
2017-09-08 09:06:50 +0200

    prepareobject update and buildmap to assign integer key values to payload

--- src/prepareobject.js
+++ src/prepareobject.js
@@ -1,29 +1,62 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/*
-var timespan = require('./lib/timespan');
-var once = require('lodash.once');
-var xtend = require('xtend');
-*/
 var cbor = require('cbor');
-var numbers = ["1", "2", "3", "4", "5", "6", "7"];
-var claims = ['iss', 'sub', 'aud', 'exp', 'nbf', 'iat', 'cti'];
-var payload = {
-    iss: "test",
-    exp: 1234567890
-};
-var i = 0;
-function prepareItem(obj) {
+var base64 = require('base-64');
+var claims = { iss: 1, sub: 2, aud: 3, exp: 4, nbf: 5, iat: 6, cti: 7 };
+var payload = { iss: 'test', exp: 'abc' };
+//var headerparameters = ['alg', 'crit', 'content_type', 'kid', 'IV', 'Partial_IV', 'counter_signature'];
+var header = { alg: 4 };
+function buildMap(obj) {
     for (var _i = 0, _a = Object.keys(obj); _i < _a.length; _i++) {
         var key = _a[_i];
-        var handle = claims.indexOf(key) + 1;
-        obj[handle] = obj[key];
-        delete (obj[key]);
+        if (Object.keys(claims) == (Object.keys(payload[key]))) {
+            console.log(Object.keys(claims));
+            console.log((Object.keys(payload[key])));
+            //    if( [1, 2, 3, 4, 5, 6, 7].includes(claims.indexOf(key)+1)){
+            //       var test = Object.keys(obj).reduce((map, key) => map.set(claims.indexOf(key)+1, obj[key]), new Map());
+            test = cbor.encode(test);
+            /*     }else {
+                     throw new Error ('invalid payload key!');
+                 }
+             }else {
+                 throw new Error ('invalid payload key!');
+             }
+         }
+         */
+            return test;
+        }
     }
-    return obj;
 }
-var x = prepareItem(payload);
-var old = JSON.stringify(x).replace(/"/g, '');
-var newArray = JSON.parse(old);
-console.log(newArray);
+function preparepayload(obj) {
+    //replacing claims with their respective ids
+    for (var _i = 0, _a = Object.keys(obj); _i < _a.length; _i++) {
+        var key = _a[_i];
+        /*       var handle = claims.indexOf(key)+1;
+               obj[handle] = obj[key];
+               delete(obj[key]);
+       }*/
+        return obj;
+    }
+}
+function prepareItem(obj, header) {
+    obj = buildMap(obj);
+    //preparing header
+    for (var _i = 0, _a = Object.keys(header); _i < _a.length; _i++) {
+        var key = _a[_i];
+        if (header[key] != 4) {
+            return (console.log('wrong algorithm, try 17'));
+        }
+        else {
+            //putting header and payload together
+            var COSE_Mac0 = [header, obj];
+        }
+    }
+    //encoding the newly assembled Object
+    return cbor.encode(COSE_Mac0);
+}
+function wrapItem(obj) {
+    //wrap obj..
+}
+var map = buildMap(payload);
+var y = prepareItem(payload, header);
+console.log(y);
+y = wrapItem(y);
 //# sourceMappingURL=prepareobject.js.map
--- src/prepareobject.ts
+++ src/prepareobject.ts
@@ -1,29 +1,65 @@
-import cbor from "CBOR"
-/*
-var timespan = require('./lib/timespan');
-var once = require('lodash.once');
-var xtend = require('xtend');
-*/
-
 var cbor = require('cbor');
+var base64 = require('base-64');
 
-var numbers = ["1","2","3","4","5","6","7"];
-var claims = ['iss', 'sub', 'aud', 'exp', 'nbf', 'iat', 'cti'];
-var payload = {
-    iss: "test",
-    exp: 1234567890
-}
-var i = 0;
-function prepareItem(obj) {
+var claims = {iss: 1, sub: 2, aud: 3, exp: 4, nbf: 5 , iat: 6, cti: 7};
+var payload = {iss: 'test',exp: 'abc'}
+//var headerparameters = ['alg', 'crit', 'content_type', 'kid', 'IV', 'Partial_IV', 'counter_signature'];
+var header = { alg: 4};
+
+function buildMap(obj) {
     for (var key of Object.keys(obj)){
-            var handle = claims.indexOf(key)+1;
+        if((Object.keys(obj[key])).indexOf(Object.keys(claims)) {
+            console.log(Object.keys(claims));
+            console.log((Object.keys(payload[key])));
+        //    if( [1, 2, 3, 4, 5, 6, 7].includes(claims.indexOf(key)+1)){
+         //       var test = Object.keys(obj).reduce((map, key) => map.set(claims.indexOf(key)+1, obj[key]), new Map());
+                test = cbor.encode(test);
+       /*     }else {
+                throw new Error ('invalid payload key!');
+            }
+        }else {
+            throw new Error ('invalid payload key!');
+        }
+    }
+    */
+    return test;
+    }
+}
+}
+
+function preparepayload(obj) {
+    //replacing claims with their respective ids
+    for (var key of Object.keys(obj)){
+     /*       var handle = claims.indexOf(key)+1;
             obj[handle] = obj[key];
             delete(obj[key]);
-    }
+    }*/
     return obj;
+    }
 }
-var x = prepareItem(payload);
-var old = JSON.stringify(x).replace(/"/g,'');
-var newArray = JSON.parse(old);
-console.log(newArray);
 
+
+function prepareItem(obj, header){
+    obj = buildMap(obj);
+    //preparing header
+    for (var key of Object.keys(header)){
+        if (header[key] != 4){
+            return (console.log('wrong algorithm, try 17'));
+        }
+        else{
+            //putting header and payload together
+            var COSE_Mac0 = [ header, obj];
+        }
+    }
+    //encoding the newly assembled Object
+    return cbor.encode(COSE_Mac0);
+}
+
+
+function wrapItem(obj){
+    //wrap obj..
+}
+const map = buildMap(payload);
+var y = prepareItem(payload, header);
+console.log(y);
+y = wrapItem(y);
