41cfb51c348d801a64d3cfe403b91c3f9413a422
Lukas Dietrich <lukasdietrich@netnexus.de>
2017-09-13 09:08:02 +0200

    Payload is now being created correctly. Implemented cose create fnc to create CWT Hex string

--- src/prepareobject.ts
+++ src/prepareobject.ts
@@ -1,9 +1,11 @@
 var cbor = require('cbor');
-var cose = require('cose-js');
+const cose = require('cose-js');
+const jsonfile = require('jsonfile');
+const base64url = require('base64url');
 //var base64 = require('base-64');
 
 var claims = {iss: 1, sub: 2, aud: 3, exp: 4, nbf: 5 , iat: 6, cti: 7};
-var payload = {iss: "test" , 3: "abc", undefined: "wasd", aud : "try", 123920: "mk"}
+var payload = {iss: "coap://as.example.com" , sub: "erikw", aud: "coap://light.example.com", exp : 1444064944, nbf: 1443944944, iat: 1443944944, cti: Buffer.from('0b71','hex')}
     
 //var headerparameters = ['alg', 'crit', 'content_type', 'kid', 'IV', 'Partial_IV', 'counter_signature'];
 var header = { alg: 4};
@@ -23,7 +25,9 @@ function buildMap(obj:any): Map<string | number, any> {
             }
         }
     }
+
 return cbor.encode(m);
+
 }
 
 function preparepayload(obj) {
@@ -51,16 +55,18 @@ function prepareItem(obj, header){
         }
     }
     //encoding the newly assembled Object
+    console.log(COSE_Mac0);
     return cbor.encode(COSE_Mac0);
 }
-
-
-
-
-
 function wrapItem(obj){
     //wrap obj..
 }
-const tester = buildMap(payload);
-var tester2 = cose.doMac(payload,17);
-console.log((tester));
+var tester = buildMap(payload);
+
+cose.mac.create(
+    { 'p':{"alg":"SHA-256_64"},'u':{}},
+    tester,
+    [{'key':Buffer.from("403697de87af64611c1d32a05dab0fe1fcb715a86ab435f1ec99192d79569388", 'hex')}])
+  .then((buf) => {
+    console.log(buf.toString('hex'));
+});
